8101455a4e38b28c1ee47579a2453bf36a1cfea4
Bilal Dilbar <bilal.dilbar54@gmail.com>
2023-03-31 21:18:09 -0400

    Initial Commit

---
+++ MAPD724-W2023-ICE-9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAPD724-W2023-ICE-9
+//
+//  Created by Muhammad Bilal Dilbar on 2023-03-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
